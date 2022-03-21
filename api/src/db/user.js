3facf4b760dc2eb338a51594ed058028e1168655
delinuxist <delinuxist@gmail.com>
2022-03-21 08:52:31 +0000

    slight edit

--- api/src/db/user.js
+++ api/src/db/user.js
@@ -1,20 +1,21 @@
-const nanoid = require('nanoid')
+const nanoid = require("nanoid");
 
-const createUserModel = db => {
+const createUserModel = (db) => {
   return {
+    findMany(filter) {
+      return db.get("user").filter(filter).value();
+    },
     findOne() {
-      return db.get('user')
-        .value()
+      return db.get("user").value();
     },
 
     create(user) {
-      const newUser = {id: nanoid(), createdAt: Date.now(), ...user}
-      db.set('user', newUser)
-        .write()
-  
-      return newUser
-    }
-  }
-}
+      const newUser = { id: nanoid(), createdAt: Date.now(), ...user };
+      db.set("user", newUser).write();
 
-module.exports = createUserModel
+      return newUser;
+    },
+  };
+};
+
+module.exports = createUserModel;
