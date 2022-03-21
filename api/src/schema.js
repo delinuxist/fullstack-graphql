9f23572b216bf0705ae3a608e1ae50b6a2d9864d
delinuxist <delinuxist@gmail.com>
2022-03-21 08:53:40 +0000

    typeDefs for apollo server

--- api/src/schema.js
+++ api/src/schema.js
@@ -1,11 +1,37 @@
-const { gql } = require('apollo-server')
+const { gql } = require("apollo-server");
 
 /**
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type Query {
+    pets: [Pet]!
+    pet(id: ID!): Pet!
+  }
 
+  type Mutation {
+    createPet(input: PetInput): Pet!
+  }
+
+  type Pet {
+    id: ID!
+    name: String!
+    type: String!
+    img: String!
+    createdAt: String!
+    owner: User!
+  }
+
+  input PetInput {
+    name: String!
+    type: String!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    pets: [Pet]!
+  }
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
