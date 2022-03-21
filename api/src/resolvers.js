ce45c86b10af6e93a1b7b9291b6a1da5afcd7acd
delinuxist <delinuxist@gmail.com>
2022-03-21 08:53:11 +0000

    all resolvers for apollo server

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,32 @@
 
 module.exports = {
   Query: {
-    
+    pets: (_, __, { models }) => {
+      return models.Pet.findMany();
+    },
+    pet: (_, { id }, { models }) => {
+      return models.Pet.findOne({ id });
+    },
   },
   Mutation: {
-    
+    createPet: (_, { input }, { models }) => {
+      return models.Pet.create(input);
+    },
   },
+
   Pet: {
     img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
+      return pet.type === "DOG"
+        ? "https://placedog.net/300/300"
+        : "http://placekitten.com/300/300";
+    },
+    owner: (pet, __, { models }) => {
+      return models.User.findOne(pet.user);
+    },
   },
   User: {
-    
-  }
-}
+    pets: (_, __, { models }) => {
+      return models.Pet.findMany();
+    },
+  },
+};
